6054266d6faaac146fd3c4a92f693241ee77bfe1
yooju00 <yooju00@naver.com>
2024-12-12 20:51:50 +0900

    feat(common): Chip 버튼 컴포넌트 구현

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {ReactNode} from "react";
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant?: 'default' | 'disabled' | 'master';
     children: React.ReactNode;
+    className?: string;
+}
+
+export interface ChipProps {
+    children: ReactNode;
+    size?: 'small' | 'large';
+    icon?: ReactNode;
     className?: string;
 }
