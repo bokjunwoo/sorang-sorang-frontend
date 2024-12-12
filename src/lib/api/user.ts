6b92ade74063c5e8d8eac5ebeb268ee91b0cf4aa
JUNU <bokjunwoo@gmail.com>
2024-12-13 05:54:56 +0900

    feat(explore): 리액트쿼리 목데이터

---
+++ src/lib/api/user.ts
@@ -0,0 +1,20 @@
+import { userInfo } from '@/app/data/userInfo';
+import { UserInfo } from '@/types/user';
+import { useQuery } from '@tanstack/react-query';
+
+export const getExplore = async (): Promise<UserInfo[]> => {
+  try {
+    return userInfo;
+  } catch (error) {
+    console.error('에러 발생:', error);
+    throw new Error('목데이터 로드 실패');
+  }
+};
+
+export const useExplore = () => {
+  return useQuery<UserInfo[], Error>({
+    queryKey: ['explore'], // 쿼리 키
+    queryFn: getExplore, // 데이터 요청 함수
+    retry: false, // 실패 시 재시도 여부
+  });
+};
---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+export type UserInfo = {
+  id: number;
+  location: string;
+  name: string;
+  gender: string;
+  title: string;
+  keyword: string;
+  description: string;
+  full_description: string;
+};
