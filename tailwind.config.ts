88479cb02e3cf3c230c9b4e25739f7e475d4ab56
yooju00 <yooju00@naver.com>
2024-12-12 17:46:44 +0900

    design: 디자인 시스템 색상 및 폰트 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,57 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        brand: {
+          bg1: "#F6EAD7",
+          bg2: "#EBDDC6",
+          white: "#F8FEFF",
+          black: "#3C3C3C",
+          gray: "#5C5C5C",
+          0: "#FFFFFF",
+          1: "#000000",
+          primary1: "#FF9553",
+          primary2: "#FA8339",
+          primary3: "#E46719",
+        },
+
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+        hakgyo: ['HakgyoansimWoojuR', 'serif'],
+      },
+
+      fontSize: {
+        'pretendard-s': ['14px', {
+          lineHeight: '22px',
+          letterSpacing: '-0.02em',
+          fontWeight: '500',
+        }],
+        'pretendard-m': ['18px', {
+          lineHeight: '30px',
+          letterSpacing: '-0.02em',
+          fontWeight: '500',
+        }],
+        'pretendard-l': ['28px', {
+          lineHeight: '40px',
+          letterSpacing: '-0.02em',
+          fontWeight: '700',
+        }],
+        'hakgyo-s': ['12px', {
+          lineHeight: '22px',
+          letterSpacing: '0',
+        }],
+        'hakgyo-m': ['16px', {
+          lineHeight: '22px',
+          letterSpacing: '0',
+        }],
+        'hakgyo-l': ['20px', {
+          lineHeight: '28px',
+          letterSpacing: '0',
+        }],
+      }
     },
   },
   plugins: [],
