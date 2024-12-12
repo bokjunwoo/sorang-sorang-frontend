804e2c5d53e6181217697ed41cadd1f14e1f387b
yooju00 <yooju00@naver.com>
2024-12-13 05:06:11 +0900

    feat(speech): SpeechPage UI 구현

--- src/app/master/layout.tsx
+++ src/app/master/layout.tsx
@@ -1,14 +1,25 @@
+'use client'
+
 import Image from 'next/image';
+import {usePathname} from "next/navigation";
 
 interface LayoutProps {
     children: React.ReactNode;
 }
 
-export default function MasterLayout({ children }: LayoutProps) {
+export default function MasterLayout({children}: LayoutProps) {
+    const pathname = usePathname();
+
+    const getBackgroundImage = () => {
+        if (pathname === '/master/info') return '/info_bg.svg';
+        if (pathname === '/master/speech') return '/speech_bg.svg';
+        return '/master_bg.svg';
+    };
+
     return (
         <div className="fixed inset-0 overflow-hidden">
             <Image
-                src="/master_bg.svg"
+                src={getBackgroundImage()}
                 alt="Background"
                 fill
                 className="object-cover -z-10"
