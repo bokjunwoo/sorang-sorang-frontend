cdf8d2d84a545431280ca436f515c1417da695f5
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2024-12-13 10:13:05 +0900

    Update master.ts

--- src/store/master.ts
+++ src/store/master.ts
@@ -1,24 +1,28 @@
-import {create} from "zustand";
-import {MasterState} from "@/types/master";
+import { create } from 'zustand';
+import { MasterState } from '@/types/master';
 
 export const masterStore = create<MasterState>((set) => ({
-    masterInfo: {
+  masterInfo: {
+    name: '',
+    gender: '',
+    region: '',
+    keyword: '',
+    number: '',
+  },
+
+  setMasterInfo: (newMasterInfo) =>
+    set((state) => ({
+      masterInfo: { ...state.masterInfo, ...newMasterInfo },
+    })),
+
+  resetMasterInfo: () =>
+    set({
+      masterInfo: {
         name: '',
         gender: '',
         region: '',
         keyword: '',
-    },
-
-    setMasterInfo: (newMasterInfo) => set((state) => ({
-        masterInfo: { ...state.masterInfo, ...newMasterInfo }
-    })),
-
-    resetMasterInfo: () => set({
-        masterInfo: {
-            name: '',
-            gender: '',
-            region: '',
-            keyword: '',
-        }
+        number: '',
+      },
     }),
 }));
