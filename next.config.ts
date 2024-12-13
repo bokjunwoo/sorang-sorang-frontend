83d8244951a2acb2ba05d3d6cdd76f88433b7a68
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2024-12-13 10:01:25 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 빌드 중에 ESLint 검사를 무시하도록 설정
+    ignoreDuringBuilds: true,
+  },
+  // 다른 설정 옵션들...
 };
 
 export default nextConfig;
