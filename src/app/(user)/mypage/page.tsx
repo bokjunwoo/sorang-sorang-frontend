a91d689977bf9e594a351261a9fba2628bbe3dba
JUNU <bokjunwoo@gmail.com>
2024-12-13 09:40:22 +0900

    feat: 이름표 기능 구현

--- src/app/(user)/mypage/page.tsx
+++ src/app/(user)/mypage/page.tsx
@@ -3,6 +3,7 @@ import Icon from '@/components/user/mypage/Icon';
 import Image from 'next/image';
 import New from '@/components/user/mypage/New';
 import Count from '@/components/user/mypage/Count';
+import Name from '@/components/user/mypage/Name';
 
 export default function page() {
   return (
@@ -12,9 +13,7 @@ export default function page() {
         {/* 좌측 부분: 이름 텍스트와 name 이미지 */}
         <div className='relative flex items-center justify-center'>
           {/* 이름 텍스트 (이미지 위에 절대 위치) */}
-          <div className='absolute top-2 left-14 text-white font-pretendard text-center mb-2 font-bold'>
-            김순자
-          </div>
+          <Name />
 
           {/* 이름 이미지 */}
           <Image src={'/name.svg'} alt={'이름'} width={120} height={120} />
