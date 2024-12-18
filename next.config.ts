47acd6f4a1b809f6ceb9071172d8afa026741204
yooju00 <yooju00@naver.com>
2024-12-19 01:47:40 +0900

    build: Github Action 배포 설정

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',  // Static HTML 내보내기를 위한 설정
+  images: {
+    unoptimized: true,  // GitHub Pages 배포를 위한 이미지 설정
+  },
+  assetPrefix: './',  // 정적 파일 경로 설정
   eslint: {
     // 빌드 중에 ESLint 검사를 무시하도록 설정
     ignoreDuringBuilds: true,
   },
-  // 다른 설정 옵션들...
 };
 
 export default nextConfig;
