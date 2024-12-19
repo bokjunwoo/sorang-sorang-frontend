1cf0969d5081bf5974a07fecf96c83cd527612e7
yooju00 <yooju00@naver.com>
2024-12-19 17:28:31 +0900

    feat(mypage): 신규 스토리 알림 기능 구현

--- src/components/user/mypage/NewStoryNotification.tsx
+++ src/components/user/mypage/NewStoryNotification.tsx
@@ -6,56 +6,54 @@ import Link from 'next/link';
 import {getLatestStory} from "@/lib/api/story";
 
 export default function NewStoryNotification() {
-  const [isVisible, setIsVisible] = useState<boolean>(true);
+  const [hasStory, setHasStory] = useState(false);
   const [masterInfo, setMasterInfo] = useState<{
+    id: number;
     name: string;
     gender: '할머니' | '할아버지';
   } | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const stored = localStorage.getItem('new');
-    if (stored) {
-      setIsVisible(JSON.parse(stored));
-    }
-
     const fetchLatestStory = async () => {
       try {
         const data = await getLatestStory();
-        setMasterInfo({
-          name: data.masterName,
-          gender: data.masterGender
-        });
+
+        // 스토리 데이터가 있으면 상태 업데이트
+        if (data) {
+          setHasStory(true);
+          setMasterInfo({
+            id: data.id,
+            name: data.name,
+            gender: data.gender
+          });
+        }
       } catch (error) {
         console.error('Failed to fetch latest story:', error);
-      } finally {
-        setIsLoading(false);
       }
     };
 
     fetchLatestStory();
   }, []);
 
+  // 스토리가 있고 masterInfo가 있을 때만 알림 표시
   return (
-    <div className='relative px-4 mt-12'>
-      {/* 알림 메시지 컨테이너 */}
-      <div className='flex justify-center'>
-        {!isLoading && masterInfo && isVisible && (
-            <Link href={'/story/1'}>
-              <div className='font-hakgyo text-hakgyo-l text-brand-black bg-white p-4 rounded-2xl'>
-                <div>{masterInfo.name} {masterInfo.gender}의</div>
-                <div>새로운 이야기가 도착했어요!</div>
-              </div>
-            </Link>
-        )}
-      </div>
+      <div className='relative px-8 mt-12'>
+        <div className='flex justify-between items-start gap-8'>
+          {hasStory && masterInfo && (
+              <Link href={`/story/${masterInfo.id}`} passHref>
+                <div className='font-hakgyo text-hakgyo-m text-brand-black bg-white p-4 rounded-2xl'>
+                  <div>{masterInfo.name} {masterInfo.gender}의</div>
+                  <div>새로운 이야기가 도착했어요!</div>
+                </div>
+              </Link>
+          )}
+        </div>
 
-      {/* 보따리 이미지 - 오른쪽 고정 */}
-      <div className='absolute right-4 -top-2'>
-        <Link href={'/explore'}>
-          <Image src={'/pocket.svg'} alt={'보따리열기'} width={80} height={80}/>
-        </Link>
+        <div className='absolute right-4 top-0'>
+          <Link href={'/explore'}>
+            <Image src={'/pocket.svg'} alt={'보따리열기'} width={80} height={80}/>
+          </Link>
+        </div>
       </div>
-    </div>
   );
 }
