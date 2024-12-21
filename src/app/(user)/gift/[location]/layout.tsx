f9a8641183d2a0ba4a456e41e8d3e918ef11d7ed
JUNU <bokjunwoo@gmail.com>
2024-12-22 00:43:02 +0900

    refactor(gift): gift 레이아웃 & 페이지 수정

--- src/app/(user)/gift/[location]/layout.tsx
+++ src/app/(user)/gift/[location]/layout.tsx
@@ -1,13 +1,28 @@
-export default function GiftLayout({
+import Image from 'next/image';
+
+export default function ExploreLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <div className='absolute inset-0 bg-black opacity-50 z-10'></div>
+    <div className='relative min-h-screen w-full overflow-hidden'>
+      {/* 배경 이미지 */}
+      <div className='absolute inset-0 -z-10'>
+        <Image
+          src='/bg/user_bg.svg'
+          alt='Background'
+          fill
+          className='object-cover'
+          priority
+        />
+      </div>
 
-      <div className='relative z-20'>{children}</div>
+      {/* 검정색 오버레이 */}
+      <div className='absolute inset-0 -z-10 bg-black opacity-50'></div>
+
+      {/* children */}
+      <div className='relative z-10'>{children}</div>
     </div>
   );
 }
